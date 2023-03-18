7330c190451ac32d4762f6ebf14f7eaf03c9332e
Tess <tess.hsu@gmail.com>
2023-03-18 09:32:51 +0100

    feat(listing): add pricing

--- typescript-serverless/src/functions/price/handler.ts
+++ typescript-serverless/src/functions/price/handler.ts
@@ -1,13 +1,31 @@
 import { functionHandler } from "@/libs/function";
-import { Price } from "@/types.generated";
+import {Listing, ListingWrite, Price} from "@/types.generated";
+import {EntityNotFound, NotFound} from "@/libs/errors";
+import {getListingPriceRepository} from "@/repositories/listing_prices";
 
-export const getListingPrices = functionHandler<Price[]>(async () => {
-  // Replace this with your implementation.
-  return {
-    statusCode: 200,
-    response: [
-      { price_eur: 100000, created_date: "2023-01-12T09:23:36Z" },
-      { price_eur: 150000, created_date: "2023-01-17T08:17:32Z" },
-    ],
-  };
+
+export const getListingPrices = functionHandler<Price[]>(
+    async (event, context) => {
+
+  // TODO Replace this with your implementation.
+  // return {
+  //   statusCode: 200,
+  //   response: [
+  //     { price_eur: 100000, created_date: "2023-01-12T09:23:36Z" },
+  //     { price_eur: 150000, created_date: "2023-01-17T08:17:32Z" },
+  //   ],
+  // };
+  try {
+    const listing_prices = await getListingPriceRepository(context.postgres).getListingPrices(
+        parseInt(event.pathParameters.id)
+    );
+
+    return { statusCode: 200, response: listing_prices };
+  } catch (e) {
+    if (e instanceof EntityNotFound) {
+      throw new NotFound(e.message);
+    }
+
+    throw e;
+  }
 });
---
+++ typescript-serverless/src/repositories/listing_prices.ts
@@ -0,0 +1,64 @@
+import PostgresClient from "serverless-postgres";
+import {ListingWrite, Price} from "@/types.generated";
+import { extractVariables } from "@/libs/postgres";
+import { EntityNotFound } from "@/libs/errors";
+
+type ListingPriceTableRow = {
+  id?: number;
+  listing_id?: number;
+  created_date: Date;
+  price: number;
+};
+
+function tableRowToListingPrice(row: ListingPriceTableRow): Price {
+  return {
+    price_eur: row.price,
+    created_date: row.created_date.toISOString()
+  };
+}
+
+function listingPriceToTableRow(
+    price: number,
+  createdDate: Date,
+  listing_id?: number
+): ListingPriceTableRow {
+  return {
+    listing_id: listing_id,
+    price: price,
+    created_date: createdDate,
+  };
+}
+
+export function getListingPriceRepository(postgres: PostgresClient) {
+  return {
+
+    async getListingPrices(listingId: number) : Promise<Price[]> {
+      const queryString = `SELECT * FROM listing_price WHERE listing_id = $1`;
+      const queryValues = [listingId];
+
+      const result = await postgres.query(queryString, queryValues);
+
+      return result.rows.map(tableRowToListingPrice);
+    },
+
+    async insertListingPrice(listing_id: number, listing: ListingWrite) {
+      const tableRow = listingPriceToTableRow(listing.latest_price_eur, new Date(), listing_id);
+
+      const {
+        columns,
+        variables,
+        values: queryValues,
+      } = extractVariables(tableRow);
+
+      const queryString = `
+        INSERT INTO listing_price (${columns.join(",")})
+        VALUES(${variables})
+        RETURNING *
+      `;
+      const result = await postgres.query(queryString, queryValues);
+
+      return tableRowToListingPrice(result.rows[0]);
+    },
+
+  };
+}
