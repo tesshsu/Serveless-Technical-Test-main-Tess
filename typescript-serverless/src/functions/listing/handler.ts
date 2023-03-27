1fc5c66c060de9d0713569f1d1927e53c1a25410
tess <tess@furuke.com>
2023-03-27 17:40:17 +0200

    Enhancement binding error in insertLsitngPrice and change binding upload over 10 times error in update listing endpoint

--- typescript-serverless/src/functions/listing/handler.ts
+++ typescript-serverless/src/functions/listing/handler.ts
@@ -1,7 +1,7 @@
 import { functionHandler } from "@/libs/function";
 import { getRepository } from "@/repositories/listings";
 import { Listing, ListingWrite } from "@/types.generated";
-import { EntityNotFound, NotFound } from "@/libs/errors";
+import { EntityNotFound, NotFound, BadRequest } from "@/libs/errors";
 import { getListingPriceRepository } from "@/repositories/listing_prices";
 
 export const getListings = functionHandler<Listing[]>(
@@ -34,11 +34,27 @@ export const updateListing = functionHandler<Listing, ListingWrite>(
           parseInt(event.pathParameters.id),
           event.body
         );
+
       // Once update listing price sync insert to listing price table
-        await getListingPriceRepository(context.postgres).insertListingPrice(
+        const response = await getListingPriceRepository(context.postgres).insertListingPrice(
             parseInt(event.pathParameters.id),
             event.body
         );
+
+        console.log('response: ', response);
+
+      if (!response) {
+        throw new BadRequest(`Inserting listing price failed`);
+      }
+
+      // If insert Listing price row over 10, throw error
+        const responseListingPrice = await getListingPriceRepository(context.postgres).getListingPrices(
+          parseInt(event.pathParameters.id)
+        )
+      
+      if(responseListingPrice.length > 10) {
+         throw new BadRequest(`Listing price update over 10 times`)
+      }
 
       return { statusCode: 201, response: listing };
     } catch (e) {
